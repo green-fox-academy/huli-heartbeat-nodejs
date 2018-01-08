f2f184152e38b82f9df03d6b06ba167df1992c32
AHolcsik <annamaria.holcsik@gmail.com>
2018-01-08 16:44:10 +0100

    fix: test adjusted based on issue in supertest

--- test.spec.js
+++ test.spec.js
@@ -20,14 +20,14 @@ describe('GET /heartbeat', function() {
     it('respond with json', function(done) {
         request(app)
             .get('/heartbeat')
-            // .set('Accept', 'application/json')
-            // .expect('Content-Type', /json/)
-            // .expect(200)
-            .end(function(err, res) {
-                if (err) return done(err);
+            .expect(200)
+            .then(function(){
                 done()
             })
-        });
+            .catch(err => {
+                done(err)
+            })
+        })
     })
 
 
