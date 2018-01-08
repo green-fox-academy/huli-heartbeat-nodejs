952b15a281eb7eee4208db50f0e2d586ccf3910b
AHolcsik <annamaria.holcsik@gmail.com>
2018-01-08 16:23:11 +0100

    refractor: modified to config file

--- test.spec.js
+++ test.spec.js
@@ -3,18 +3,18 @@
 const request = require('supertest')
 const express = require('express')
 const mysql = require('mysql')
+const config = require('./config')
 const heartbeat = require('./index')
 const app = express()
 
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'imanoodle',
-    database: 'fox_player'
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
 })
 
 app.get('/heartbeat', heartbeat(connection))
-
 
 describe('GET /heartbeat', function() {
     it('respond with json', function(done) {
