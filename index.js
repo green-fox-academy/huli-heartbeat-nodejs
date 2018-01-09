59243146da3fc71dce65069d0394e455c45ed55a
AHolcsik <annamaria.holcsik@gmail.com>
2018-01-09 15:18:41 +0100

    refractor: excess code removed

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ function heartbeat(connection) {
         connection.query("SHOW TABLES;", function(error, result) {
             if (error) {
                 res.status(500)
-                // res.json(error)
-                //res.status(500).json
             }
             res.json({result:"okay"})
         })
