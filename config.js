18b395823473117355a9f28df6b5db97bfdceaa4
AHolcsik <annamaria.holcsik@gmail.com>
2018-01-17 13:53:21 +0100

    fix on configuration

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 'use strict'
 
 let config = {
-    host: $MYSQL_HOST,
-    user: $MYSQL_USER,
-    password: $MYSQL_PASSWORD,
-    database: $MYSQL_DATABASE
+    host: '$MYSQL_HOST',
+    user: '$MYSQL_USER',
+    password: '$MYSQL_PASSWORD',
+    database: '$MYSQL_DATABASE'
 }
 
 module.exports = config
