7bff19d706017f7a29ad5e4a4cdb1045b39d13b4
AHolcsik <annamaria.holcsik@gmail.com>
2018-01-18 09:44:41 +0100

    fix: configuration adjusted

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 'use strict'
 
 let config = {
-    host: '$MYSQL_HOST',
-    user: '$MYSQL_USER',
-    password: '$MYSQL_PASSWORD',
-    database: '$MYSQL_DATABASE'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 }
 
 module.exports = config
